cabafbe34715903263db4ac92d4fdd59925c4b1a
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-12-27 23:50:11 +0800

    feat(crates/server): add `caracal-server`

---
+++ crates/server/src/config.rs
@@ -0,0 +1,61 @@
+use std::{collections::HashMap, net::SocketAddr, path::PathBuf, time::Duration};
+
+use caracal_base::profile::{minio::MinioAlias, ssh::SshConfig};
+
+#[derive(Clone, Debug)]
+pub struct Config {
+    pub task_scheduler: TaskSchedulerConfig,
+
+    pub ssh_servers: HashMap<String, SshConfig>,
+
+    pub minio_aliases: HashMap<String, MinioAlias>,
+
+    pub grpc_listen_address: Option<SocketAddr>,
+
+    pub grpc_local_socket: Option<PathBuf>,
+
+    pub grpc_access_token: Option<String>,
+
+    pub dbus: DBusConfig,
+
+    pub metrics: MetricsConfig,
+}
+
+#[derive(Clone, Debug)]
+pub struct TaskSchedulerConfig {
+    pub http: HttpConfig,
+
+    pub concurrent_number: usize,
+}
+
+#[derive(Clone, Debug)]
+pub struct HttpConfig {
+    pub user_agent: String,
+
+    pub concurrent_connections: u16,
+}
+
+#[derive(Clone, Debug)]
+pub struct DBusConfig {
+    pub enable: bool,
+
+    pub identifier: Option<String>,
+}
+
+#[derive(Clone, Debug)]
+pub struct MetricsConfig {
+    pub enable: bool,
+
+    pub listen_address: SocketAddr,
+}
+
+#[derive(Clone, Debug)]
+pub struct DesktopNotificationConfig {
+    pub enable: bool,
+
+    pub icon: PathBuf,
+
+    pub timeout: Duration,
+
+    pub long_plaintext_length: usize,
+}
