0eaaf9d3eb4765112067ec55045f1f43c253a23b
xrelkd <46590321+xrelkd@users.noreply.github.com>
2023-12-31 06:58:11 +0800

    feat(crates/server): add more log messages

--- crates/server/src/lib.rs
+++ crates/server/src/lib.rs
@@ -36,6 +36,15 @@ pub async fn serve_with_shutdown(
     let lifecycle_manager = LifecycleManager::<Error>::new();
 
     let (task_scheduler, task_scheduler_worker) = {
+        tracing::info!(
+            "Setting {} as default output directory",
+            task_scheduler.default_output_directory.display()
+        );
+        tracing::info!("Setting {} as default HTTP user-agent", task_scheduler.http.user_agent);
+        tracing::info!(
+            "Setting concurrent number of a task to {}",
+            task_scheduler.http.concurrent_connections
+        );
         let downloader_factory = DownloaderFactory::builder()
             .context(error::BuildDownloaderFactorySnafu)?
             .http_user_agent(task_scheduler.http.user_agent)
