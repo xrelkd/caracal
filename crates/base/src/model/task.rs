26a325f3d1c1bdd8a501fa77b51af11c196d5f04
xrelkd <46590321+xrelkd@users.noreply.github.com>
2026-03-01 20:59:05 +0800

    feat(crates/base): derive `utoipa::ToSchema` for `struct`s

--- crates/base/src/model/task.rs
+++ crates/base/src/model/task.rs
@@ -2,10 +2,11 @@ use std::{fmt, path::PathBuf, time::Duration};
 
 use serde::{Deserialize, Serialize};
 use time::OffsetDateTime;
+use utoipa::ToSchema;
 
 use crate::model::Priority;
 
-#[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize, ToSchema)]
 pub enum TaskState {
     Pending,
     Downloading,
@@ -29,34 +30,44 @@ impl fmt::Display for TaskState {
     }
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, ToSchema)]
 pub struct CreateTask {
+    #[schema(value_type = String, example = "https://httpbin.org/ip")]
     #[serde(with = "crate::serde::uri")]
     pub uri: http::Uri,
 
+    #[schema(value_type = Option<String>, example = "ip.json")]
     pub filename: Option<PathBuf>,
 
+    #[schema(value_type = Option<String>, example = "/tmp")]
     pub output_directory: Option<PathBuf>,
 
+    #[schema(value_type = Option<u64>, example = "5")]
     pub concurrent_number: Option<u64>,
 
+    #[schema(value_type = Option<u64>, example = "null")]
     pub connection_timeout: Option<Duration>,
 
     pub priority: Priority,
 
+    #[schema(default, value_type = String, example = OffsetDateTime::now_utc)]
     pub creation_timestamp: OffsetDateTime,
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, ToSchema)]
 pub struct TaskStatus {
+    #[schema(value_type = u64, example = 20)]
     pub id: u64,
 
+    #[schema(value_type = String, example = "/tmp/data.txt")]
     pub file_path: PathBuf,
 
+    #[schema(value_type = u64, example = 1024)]
     pub content_length: u64,
 
     pub chunks: Vec<ProgressChunk>,
 
+    #[schema(value_type = u32, example = 5)]
     pub concurrent_number: usize,
 
     pub state: TaskState,
@@ -64,17 +75,22 @@ pub struct TaskStatus {
     pub priority: Priority,
 
     #[serde(with = "time::serde::rfc3339")]
+    #[schema(default, value_type = String, example = OffsetDateTime::now_utc)]
     pub creation_timestamp: OffsetDateTime,
 }
 
-#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize, ToSchema)]
 pub struct ProgressChunk {
+    #[schema(value_type = u64, example = 50)]
     pub start: u64,
 
+    #[schema(value_type = u64, example = 150)]
     pub end: u64,
 
+    #[schema(value_type = u64, example = 30)]
     pub received: u64,
 
+    #[schema(value_type = bool, example = false)]
     pub is_completed: bool,
 }
 
